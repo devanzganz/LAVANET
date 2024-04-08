521215f100390a8eac597116b6a0f25347e406c9
devanzganz <154262438+devanzganz@users.noreply.github.com>
2024-04-09 00:19:28 +0700

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-e959cdcd-799a-4047-a669-de911e306087/"
 
 OUTPUT_DIR="logs"
 
